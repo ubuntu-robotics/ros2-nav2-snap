35c88837763ae693e76e8a9cde36539a98f09df6
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-04-24 15:08:12 +0200

    add configuration template, make configurable via URL

--- snap/local/localization_launcher.sh
+++ snap/local/localization_launcher.sh
@@ -4,5 +4,14 @@ LOC_CONFIG_FILE="$(snapctl get localization-config)"
 MAP_FILE="$(snapctl get map)"
 SIMULATION="$(snapctl get simulation)"
 
+DEFAULT_PATH_TO_CONFIG="$SNAP_COMMON/config/localization_params_template.yaml"
+
+CONFIG_FILE=$($SNAP/bin/check_is_url $LOC_CONFIG_FILE $DEFAULT_PATH_TO_CONFIG)
+
+if [ -z "${CONFIG_FILE}" ] ; then
+    echo "Config is not a URL"
+    CONFIG_FILE=${DEFAULT_PATH_TO_CONFIG}
+fi
+
 ros2 launch nav2_bringup localization_launch.py \
-  params_file:=${LOC_CONFIG_FILE} map:=${MAP_FILE} use_sim_time:=${SIMULATION}
+  params_file:=${CONFIG_FILE} map:=${MAP_FILE} use_sim_time:=${SIMULATION}
