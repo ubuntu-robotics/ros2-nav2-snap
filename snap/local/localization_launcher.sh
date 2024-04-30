39bc570debf4abe55e2990efcaf0a14cc7f60f3a
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-04-30 16:31:14 +0200

    clean readme and fix bash indentations

--- snap/local/localization_launcher.sh
+++ snap/local/localization_launcher.sh
@@ -12,20 +12,20 @@ if [ -z "${LOC_CONFIG_FILE}" ] ; then
 fi
 
 if [ -z "${MAP_FILE}" ]; then
-  echo "No current_map.yaml found, run slam to generate one or load one via URL"
-  exit 1
+    echo "No current_map.yaml found, run slam to generate one or load one via URL"
+    exit 1
 fi
 
 # it the config-path is a URL we save it to a default local location
 # otherwise, it it's a local path we use that.
 if is_url "${LOC_CONFIG_FILE}" ; then
-  STORAGE_PATH_TO_CONFIG="$SNAP_COMMON/config/localization_params.yaml"
-  if get_url "${LOC_CONFIG_FILE}" "${STORAGE_PATH_TO_CONFIG}"; then
-    CONFIG_FILE=${STORAGE_PATH_TO_CONFIG}
-  fi
+    STORAGE_PATH_TO_CONFIG="$SNAP_COMMON/config/localization_params.yaml"
+    if get_url "${LOC_CONFIG_FILE}" "${STORAGE_PATH_TO_CONFIG}"; then
+        CONFIG_FILE=${STORAGE_PATH_TO_CONFIG}
+    fi
 else
-  echo "Config is not a URL, setting local path"
-  CONFIG_FILE=${LOC_CONFIG_FILE}
+    echo "Config is not a URL, setting local path"
+    CONFIG_FILE=${LOC_CONFIG_FILE}
 fi
 
 ros2 launch nav2_bringup localization_launch.py \
--- snap/local/map_loader.sh
+++ snap/local/map_loader.sh
@@ -11,18 +11,18 @@ if ! is_url "${MAP_YAML_URL}"; then
 fi
 
 if is_url "${MAP_YAML_URL}" ; then
-  MAP_NAME=$(basename "$MAP_YAML_URL")
-  if get_url "${MAP_YAML_URL}" "${MAP_DIRS}/${MAP_NAME}"; then
-    echo "Map yaml file downloaded successfully"
-    # create symlink to use the map
-    echo "Creating symlink"
-    rm -f ${SNAP_COMMON}/maps/current_map.yaml
-    ln -s "${MAP_DIRS}/${MAP_NAME}" ${SNAP_COMMON}/maps/current_map.yaml
-    echo "Symlink created"
-  else
-    echo "Could not download yaml file"
-    exit 1
-  fi
+    MAP_NAME=$(basename "$MAP_YAML_URL")
+    if get_url "${MAP_YAML_URL}" "${MAP_DIRS}/${MAP_NAME}"; then
+        echo "Map yaml file downloaded successfully"
+        # create symlink to use the map
+        echo "Creating symlink"
+        rm -f ${SNAP_COMMON}/maps/current_map.yaml
+        ln -s "${MAP_DIRS}/${MAP_NAME}" ${SNAP_COMMON}/maps/current_map.yaml
+        echo "Symlink created"
+    else
+        echo "Could not download yaml file"
+        exit 1
+    fi
 fi
 
 # Retrieve the map image name and format from the YAML file and download it
@@ -32,13 +32,13 @@ BASE_URL=$(dirname "$MAP_YAML_URL")
 IMAGE_MAP_URL=$BASE_URL/$IMAGE_FIELD
 
 if [ -n "${IMAGE_MAP_URL}" ]; then
-  echo $IMAGE_MAP_URL
-  if get_url "${IMAGE_MAP_URL}" "${MAP_DIRS}/${IMAGE_FIELD}"; then
-    echo "Image file downloaded successfully"
-  else
-    echo "Could not download image file"
-    exit 1
-  fi
+    echo $IMAGE_MAP_URL
+    if get_url "${IMAGE_MAP_URL}" "${MAP_DIRS}/${IMAGE_FIELD}"; then
+        echo "Image file downloaded successfully"
+    else
+        echo "Could not download image file"
+        exit 1
+    fi
 fi
 
 exec $@
